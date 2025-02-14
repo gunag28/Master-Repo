60ef3b15d82969e28ca0dfba00f2b6859a8a678e
gunag28 <gunasekarmail@gmail.com>
2025-02-14 13:50:24 +0530

    for main branch

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 '''
+
+this is modified for main branch
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
 
